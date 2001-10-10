56c40b5cea5d27020db003807486309fa5f0339f
duncan <duncan@478a9d2e-6004-0410-ac81-b4d184805fe2>
2001-10-10 15:45:57 +0000

    Working example using parallel coordinates plot

--- plugins/CategoricalPlots/categoricalPlots.c
+++ plugins/CategoricalPlots/categoricalPlots.c
@@ -25,24 +25,55 @@
 
 GtkWidget *addDisplayMenuItem(ggobid *gg, const char *label);
 
+/**
+ Callback for when the user selects the "Barplot" menu item on the
+ "Display" menu.
+  This is called with the plugin instance as the first argument and 
+  from that, we can get the ggobid instance. We have also arranged
+  that the datad of interest associated with the "Barplot" menu item
+  is available from the menu item widget. Given this, we can generate
+  the barplot.  (For now, I use the first two variables in the dataset
+  and create a parallel coordinates plot!)
+
+ */
 void
 show_barplot_display(PluginInstance *inst, GtkWidget *widget)
 {
     datad *data;
+    displayd *display;
+
+    if(ValidateGGobiRef(inst->gg, false) == NULL) {
+	return;
+    }
 
     data = (datad*) gtk_object_get_data(GTK_OBJECT(widget), "data");
-
     if(!data)
 	return;
+
+    {
+     int nselected_vars = 2;
+     gint *selected_vars;
+      selected_vars = (gint *)g_malloc(2 * sizeof(gint));
+      selected_vars[0] = 0;
+      selected_vars[1] = 1;
+
+      GGobi_newParCoords(selected_vars, nselected_vars, data, inst->gg);
+      varpanel_refresh (inst->gg);
+    }
 
     return;
 }
 
+/**
+ Add an entry to the Display menu with the label `Barplot'
+ and arrange to have show_barplot_display() called when the
+ user selects this entry.
+ */
 gboolean
-addBarplotMenuItems(ggobid *gg, GGobiPluginInfo *plugin, PluginInstance *inst)
+addBarplotMenuItems(ggobid *gg, PluginInstance *inst)
 {
 
-      /*  */
+
   GtkWidget *entry;
 
   entry = GGobi_addDisplayMenuItem(gg, "Barplot");    
