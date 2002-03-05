1c634626b5e8cfc8782d24418b3f36fc4404bc42
dfs <dfs@478a9d2e-6004-0410-ac81-b4d184805fe2>
2002-03-05 15:24:24 +0000

    Making sure the new missings plots are linked to the originals; jittering them before any displays are created; making the variables categorical, and then only including those variables which actually have missings.

--- missing.c
+++ missing.c
@@ -167,37 +167,93 @@ missing_rejitter (gint *vars, gint nvars, datad *d, ggobid *gg) {
 
 /*
  * For the datad currently selected in gg->impute.notebook,
- * generate a new datad using d->missing.
+ * generate a new datad using d->missing.  Maybe I should only
+ * create missingness variables for those variables which have
+ * missing values ...
 */
 void missings_datad_cb (GtkWidget *w, ggobid *gg)
 {
   GtkObject *obj = GTK_OBJECT(gg->impute.window);
   GtkWidget *clist = get_clist_from_object (obj);
   datad *d = (datad *) gtk_object_get_data (GTK_OBJECT (clist), "datad");
+  static gchar *lnames[] = {"present", "missing"};
 
   if (d && d->nmissing > 0) {
     GtkWidget *notebook;
     datad *dnew;
-    gint i, j;
+    gint i, j, k;
     vartabled *vt, *vtnew;
+    gint *cols;
+    gint *cols_with_missings, ncols_with_missings;
+
+    ncols_with_missings = 0;
+    cols_with_missings = g_malloc (d->ncols * sizeof (gint));
+    for (j=0; j<d->ncols; j++) {
+      vt = vartable_element_get (j, d);
+      if (vt->nmissing)
+        cols_with_missings[ncols_with_missings++] = j;
+    }
 
     notebook = (GtkWidget *) gtk_object_get_data (obj, "notebook");
-    dnew = datad_create (d->nrows, d->ncols, gg);
+    dnew = datad_create (d->nrows, ncols_with_missings, gg);
     dnew->name = g_strdup_printf ("%s (missing)", d->name);
 
     for (i=0; i<d->nrows; i++) {
-      for (j=0; j<d->ncols; j++) {
-        dnew->raw.vals[i][j] = (gfloat) d->missing.vals[i][j];
+      for (j=0; j<ncols_with_missings; j++) {
+        k = cols_with_missings[j];
+        dnew->raw.vals[i][j] = (gfloat) d->missing.vals[i][k];
       }
     }
+
+    /*
+     * ids to support linking: if the current datad doesn't
+     * have ids, they need to be assigned.
+    */
+    if (d->rowid.id.nels == 0) {
+      rowids_alloc (d);
+      for (i=0; i<d->nrows; i++)
+        d->rowid.id.els[i] = i;
+      rowidv_init (d);
+    }
+    rowids_alloc (dnew);
+    for (i=0; i<dnew->nrows; i++)
+      dnew->rowid.id.els[i] = d->rowid.id.els[i];
+    /*rowidv_init (dnew);*/  /* called in datad_init */
+    /*-- --*/
     
+/*
+ * I'm going to make all the variables categorical.  For the moment,
+ * there can be only two categories: present (0), missing (1).  In
+ * the future, we might want to support other categories:  censored,
+ * left-censored, etc.
+*/
     vartable_alloc (dnew);
     vartable_init (dnew);
-    for (j=0; j<dnew->ncols; j++) {
-      vt = vartable_element_get (j, d);
+    for (j=0; j<ncols_with_missings; j++) {
+      k = cols_with_missings[j];
+      vt = vartable_element_get (k, d);
       vtnew = vartable_element_get (j, dnew);
       vtnew->collab = g_strdup (vt->collab);
       vtnew->collab_tform = g_strdup (vtnew->collab);
+
+      /*-- categorical variable definitions --*/
+      vtnew->categorical_p = true;
+      vtnew->nlevels = 2;
+      vtnew->level_values = NULL;
+      vtnew->level_names = g_array_new (false, false, sizeof (gchar *));
+      for (i=0; i<2; i++) {
+        vtnew->level_values = g_list_append (vtnew->level_values,
+          GINT_TO_POINTER(i));
+        g_array_append_val (vtnew->level_names, lnames[i]);
+      }
+
+      /*-- prepare to jitter, and set limits to [0,1] --*/
+      vtnew->lim_specified_p = true;  /*-- user-specified limits --*/
+      vtnew->lim_specified.min = 0.0;
+      vtnew->lim_specified_tform.min = 0.0;
+      vtnew->lim_specified.max = 1.0;
+      vtnew->lim_specified_tform.max = 1.0;
+      vtnew->jitter_factor = .2;
     }
 
     rowlabels_alloc (dnew, gg);
@@ -208,8 +264,26 @@ void missings_datad_cb (GtkWidget *w, ggobid *gg)
 
     datad_init (dnew, gg, false);
 
+    /*-- jitter the data --*/
+    /*-- forces unnecessary redisplay, unfortunately --*/
+    cols = g_malloc (dnew->ncols * sizeof (gint));
+    for (i=0; i<dnew->ncols; i++) cols[i] = i;
+    rejitter (cols, dnew->ncols, dnew, gg); 
+
+    /*-- copy the existing glyph and color --*/
+    for (i=0; i<d->nrows; i++) {
+      dnew->color.els[i] = d->color.els[i];
+      dnew->color_now.els[i] = d->color_now.els[i];
+      dnew->glyph.els[i].type = d->glyph.els[i].type;
+      dnew->glyph_now.els[i].type = d->glyph_now.els[i].type;
+      dnew->glyph.els[i].size = d->glyph.els[i].size;
+      dnew->glyph_now.els[i].size = d->glyph_now.els[i].size;
+    }
+
     /*-- this should be executed in response to any datad_added event --*/
     display_menu_build (gg);
-  }
 
+    g_free (cols);
+    g_free (cols_with_missings);
+  }
 }
