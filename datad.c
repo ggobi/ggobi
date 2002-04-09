d5a468c507f0ad6e638a6fa8cf15506db9e478de
dfs <dfs@478a9d2e-6004-0410-ac81-b4d184805fe2>
2002-04-09 21:07:19 +0000

    Removed an unnecessary function declaration, I think ...

--- datad.c
+++ datad.c
@@ -103,7 +103,6 @@ datad_init (datad *d, ggobid *gg, gboolean cleanup)
   displayd *display = NULL;
 
   if (cleanup) {
-    void varpanel_clear (datad *, ggobid *);
     varpanel_clear (d, gg);
   }
 
