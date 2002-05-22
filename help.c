3380d70a6991b244db797d750db521fbb6c691c2
duncan <duncan@478a9d2e-6004-0410-ac81-b4d184805fe2>
2002-05-22 13:58:40 +0000

    Added a message if there is no help available because CmdArgHelp.c wasn't built correctly

--- help.c
+++ help.c
@@ -10,8 +10,10 @@ showHelp()
     int i, n;
 
     n = sizeof(argHelp)/sizeof(argHelp[0]);
+    if(n == 0) {
+	fprintf(stdout, "There is no command line help for this particular compilation of GGobi. This is probably because CmdArgHelp.c was not created correctly\n");
+    }
     for(i = 0; i < n; i+=2) {
 	fprintf(stdout, "   %-20s\t%s\n", argHelp[i], argHelp[i+1]);
     }
-
 }
