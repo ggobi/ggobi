e0540a8fdf1ab6fc413ff635021a063493512a0a
duncan <duncan@478a9d2e-6004-0410-ac81-b4d184805fe2>
2003-03-20 17:06:02 +0000

    Error messages to help diagnosis

--- plugins/TestInput/testInput.c
+++ plugins/TestInput/testInput.c
@@ -12,23 +12,24 @@ test_input_description(const char * const fileName, const char * const modeName,
                              ggobid *gg, GGobiPluginInfo *info)
 {
   InputDescription *desc;
+  g_printerr("[test_input_description  started]\n");
   desc = (InputDescription*) g_malloc(sizeof(InputDescription));
   memset(desc, '\0', sizeof(InputDescription));
 
   desc->fileName = g_strdup("Test input plugin data");
   desc->mode = unknown_data;
   desc->desc_read_input = generate_data;
-
+  g_printerr("[test_input_description  finished]\n");
   return(desc);
 }
 
 gboolean 
 generate_data(InputDescription *desc, ggobid *gg, GGobiPluginInfo *plugin)
 {
-
   int nr = 10, nc = 3;
   int i, j;
   datad *d;
+  g_printerr("[generate_data %d %d]\n", nr, nc);
   d = datad_create(nr, nc, gg);
 
   for(i = 0; i < nr; i++) {
@@ -49,6 +50,7 @@ generate_data(InputDescription *desc, ggobid *gg, GGobiPluginInfo *plugin)
 	  d->raw.vals[i][j] = 100.*((float)rand()/(float)RAND_MAX);
       }
   }
+  g_printerr("[starting ggobi %d %d]\n", nr, nc);
   start_ggobi(gg, true, true);
 
   return(true);
