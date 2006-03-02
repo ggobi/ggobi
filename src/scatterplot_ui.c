c151220a13e0e32f19e7b072255c7a2f6a0654e6
dfs <dfs@478a9d2e-6004-0410-ac81-b4d184805fe2>
2006-03-02 19:46:12 +0000

    Fixed bug in working with multiple edge sets

--- src/scatterplot_ui.c
+++ src/scatterplot_ui.c
@@ -231,39 +231,39 @@ scatterplot_display_edge_menu_update (displayd *display,
 
   /*-- remove any existing submenu --*/
   if (display->edge_merge != -1) {
-	gtk_ui_manager_remove_ui(display->menu_manager, display->edge_merge);
-	if (display->edge_option_merge != -1)
-	  gtk_ui_manager_remove_ui(display->menu_manager, display->edge_option_merge);
+    gtk_ui_manager_remove_ui(display->menu_manager, display->edge_merge);
+    if (display->edge_option_merge != -1)
+      gtk_ui_manager_remove_ui(display->menu_manager, display->edge_option_merge);
     if (ne < 1) {
       /*-- destroy menu item if there are no edge sets --*/
-	  display->edge_merge = display->edge_option_merge = -1;
+      display->edge_merge = display->edge_option_merge = -1;
     }
   }
 
   /*-- then build the new menu if appropriate --*/
   if (ne) {
-	GtkAction *action = NULL;
-	GSList *group = NULL;
-	const gchar *tooltip = "Attach this edge dataset";
-	GtkActionGroup *actions = gtk_action_group_new("Edge Datasets");
+    GtkAction *action = NULL;
+    GSList *group = NULL;
+    const gchar *tooltip = "Attach this edge dataset";
+    GtkActionGroup *actions = gtk_action_group_new("Edge Datasets");
 	
-	if (display->edgeset_action_group) {
-		gtk_ui_manager_remove_action_group(display->menu_manager, 
-		  display->edgeset_action_group);
-		g_object_unref(G_OBJECT(display->edgeset_action_group));
-	}
-	gtk_ui_manager_insert_action_group(display->menu_manager, actions, -1);
-	display->edgeset_action_group = actions;
+    if (display->edgeset_action_group) {
+	gtk_ui_manager_remove_action_group(display->menu_manager, 
+	  display->edgeset_action_group);
+	g_object_unref(G_OBJECT(display->edgeset_action_group));
+    }
+    gtk_ui_manager_insert_action_group(display->menu_manager, actions, -1);
+    display->edgeset_action_group = actions;
     
 	/*-- build the menu --*/
     display->edge_merge = gtk_ui_manager_add_ui_from_string(display->menu_manager, 
 	  edge_ui, -1, NULL);
 
-	if (display->e) {
+    if (display->e) {
       gtk_ui_manager_ensure_update(display->menu_manager);
-	  display->edge_option_merge = gtk_ui_manager_add_ui_from_string(
-	 	  display->menu_manager, edge_option_ui, -1, NULL);
-	}
+      display->edge_option_merge = gtk_ui_manager_add_ui_from_string(
+	  display->menu_manager, edge_option_ui, -1, NULL);
+    }
     /*
        When there's only one edge set, indicate that on the menu
        with a single menu item naming the edge set.  Let it behave
@@ -271,39 +271,43 @@ scatterplot_display_edge_menu_update (displayd *display,
        edges.  Selecting an edge set is required.
     */
 
-	for (k=0; k<nd; k++) { 
-        e = (datad *) g_slist_nth_data (gg->d, k);
-        if (e->edge.n > 0) {
-		  gchar *lbl, *path, *name;
-		  if (ne == 1) {
-			lbl = g_strdup_printf ("Attach edge set (%s)", e->name);
-			path = "/menubar/Edges";
-			name = g_strdup ("edges");
-		  } else {
-			lbl = datasetName (e, gg);
-			path = "/menubar/Edges/Edgesets";
-			name = g_strdup_printf ("edgeset_%p", e);
-		  }
-		  if (ne == 1 || !display->e) {
-			action = gtk_action_new(name, lbl, tooltip, NULL);
-		  } else {
-			action = GTK_ACTION(gtk_radio_action_new(name, lbl, tooltip, 
-		      NULL, GPOINTER_TO_INT(e)));
-			gtk_radio_action_set_group(GTK_RADIO_ACTION(action), group);
-		    group = gtk_radio_action_get_group(GTK_RADIO_ACTION(action));
-			if (e == display->e)
-				gtk_toggle_action_set_active(GTK_TOGGLE_ACTION(action), true);
-		  }
-		  g_signal_connect(G_OBJECT(action), "activate", 
-		    G_CALLBACK(edgeset_add_cb), e);
-		  gtk_action_group_add_action(actions, action);
-		  g_object_unref(action);
-		  gtk_ui_manager_add_ui(display->menu_manager, display->edge_merge, 
-	  	    path, name, name, GTK_UI_MANAGER_MENUITEM, true);
-		  g_object_set_data(G_OBJECT(action), "display", display);
+    for (k=0; k<nd; k++) { 
+      e = (datad *) g_slist_nth_data (gg->d, k);
+      if (e->edge.n > 0) {
+	gchar *lbl, *path, *name;
+        if (resolveEdgePoints(e, d) != NULL) {
+	  if (ne == 1) {
+	    lbl = g_strdup_printf ("Attach edge set (%s)", e->name);
+	    path = "/menubar/Edges";
+	    name = g_strdup ("edges");
+	  } else {
+	    lbl = datasetName (e, gg);
+	    path = "/menubar/Edges/Edgesets";
+	    name = g_strdup_printf ("edgeset_%p", e);
+	  }
+
+  	  if (ne == 1  || !display->e) {
+	    action = gtk_action_new(name, lbl, tooltip, NULL);
+          } else {
+	    action = GTK_ACTION(gtk_radio_action_new(name, lbl, tooltip, 
+	      NULL, GPOINTER_TO_INT(e)));
+  	    gtk_radio_action_set_group(GTK_RADIO_ACTION(action), group);
+	    group = gtk_radio_action_get_group(GTK_RADIO_ACTION(action));
+	    if (e == display->e)
+	      gtk_toggle_action_set_active(GTK_TOGGLE_ACTION(action), true);
+          }
+
+	  g_signal_connect(G_OBJECT(action), "activate", 
+	    G_CALLBACK(edgeset_add_cb), e);
+	  gtk_action_group_add_action(actions, action);
+	  g_object_unref(action);
+	  gtk_ui_manager_add_ui(display->menu_manager, display->edge_merge, 
+  	    path, name, name, GTK_UI_MANAGER_MENUITEM, true);
+	  g_object_set_data(G_OBJECT(action), "display", display);
           g_free (lbl);
-		  g_free (name);
+	  g_free (name);
         }
+      }
     }
   }
 }
