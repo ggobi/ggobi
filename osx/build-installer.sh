b910ba919610230d1371eceea8d5c66e2465a149
hadley <hadley@478a9d2e-6004-0410-ac81-b4d184805fe2>
2007-01-17 17:02:09 +0000

    Further tweaks to installer script

--- osx/build-installer.sh
+++ osx/build-installer.sh
@@ -1,12 +1,16 @@
 sudo ruby build-installer.rb
 export PATH=/usr/local/gcc4.0/bin:/usr/local/gtk2/bin:$PATH
-svn co http://www.ggobi.org/svn/ggobi/ggobi/branches/ggobi-2.1.4
+wget http://www.ggobi.org/downloads/ggobi-2.1.4.tar.gz
 sudo mkdir /usr/local/ggobi
+tar xzf ggobi-2.1.4.tar.gz
 cd ggobi-2.1.4
 ./bootstrap
-./configure --prefix=/usr/local/ggobi --with-all-plugins
+LDFLAGS=-L/usr/local/gtk2/lib CFLAGS=-I/usr/local/gtk2/include ./configure --prefix=/usr/local/ggobi --with-all-plugins
 make
 sudo make install
+
+
+
 
 mkdir -p installer/usr/local
 sudo rsync -rltz /usr/local/gtk2 installer/usr/local 
@@ -14,8 +18,8 @@ sudo rsync -rltz /usr/local/ggobi installer/usr/local
 mkdir -p installer/usr/local/gcc4.0/lib/
 cp /usr/local/gcc4.0/lib/libgcc_s.1.0.dylib installer/usr/local/gcc4.0/lib/ 
 
-sudo /Developer/Tools/packagemaker -build -proj ggobi-installer.pmproj -p "Install GGobi.pkg" 
-
+sudo /Developer/Tools/packagemaker -build -proj ggobi-installer.pmproj -pGGobi.pkg
+ 	
 rm ggobi.dmg
 hdiutil create -fs HFS+ -volname GGobi -srcfolder "Install GGobi.pkg" ggobi-2.1.4.dmg
 
